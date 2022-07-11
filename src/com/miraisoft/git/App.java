608e91b39204299bd553d6b1bcc90079635bb884
Dai <dangdai.ptit@gmail.com>
2022-07-11 14:08:06 +0700

    update

--- src/com/miraisoft/git/App.java
+++ src/com/miraisoft/git/App.java
@@ -1,7 +1,15 @@
 package com.miraisoft.git;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, git");
+        Scanner scanner = new Scanner(System.in);
+        String s1 = scanner.nextLine();
+        String s2 = scanner.nextLine();
+        s1.toUpperCase();
+        s2.toUpperCase();
+        System.out.println(s1.indexOf(s2));
     }
 }
